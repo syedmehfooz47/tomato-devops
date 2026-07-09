66e012cf857468632f3779db5797e6782be8fbf1
syedmehfooz47 <syedmehfoozcs@gmail.com>
2026-07-09 12:19:18 +0530

    Update

--- Automations/update-frontend-env.sh
+++ Automations/update-frontend-env.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-INSTANCE_ID="i-095fd99dcf1b8c13c"
-
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-
-if [ -z "$ipv4_address" ] || [ "$ipv4_address" = "None" ]; then
-    echo "ERROR: Could not retrieve IP address for instance $INSTANCE_ID"
-    exit 1
-fi
-
-echo "EC2 Public IP: $ipv4_address"
+BACKEND_URL="https://food-delivery.projects.syedmehfooz.com"
 
 file_to_find="../frontend/.env.docker"
 
@@ -18,7 +9,7 @@ if [ ! -f "$file_to_find" ]; then
     exit 1
 fi
 
-sed -i -e "s|VITE_BACKEND_URL=.*|VITE_BACKEND_URL=http://${ipv4_address}:31002|g" "$file_to_find"
+sed -i -e "s|VITE_BACKEND_URL=.*|VITE_BACKEND_URL=${BACKEND_URL}|g" "$file_to_find"
 
-echo "Updated $file_to_find with VITE_BACKEND_URL=http://${ipv4_address}:31002"
+echo "Updated $file_to_find with VITE_BACKEND_URL=${BACKEND_URL}"
 cat "$file_to_find"
